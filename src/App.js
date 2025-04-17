878e5a80a05210e57fe4841b0c27efdab7f5f79a
spryce <12742003+spryce@users.noreply.github.com>
2025-04-17 13:40:55 +1000

    feat: define app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
-import SideNav from './components/SideNav';
-import ProductGrid from './components/ProductGrid';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import ProductGrid from "./components/ProductGrid";
+import ProductDetails from "./pages/ProductDetails";
+import About from "./pages/About";
+import Feedback from "./pages/Feedback";
 import data from './data.json';
+import './App.css';
 
 const App = () => {
   const { categories, series, products } = data;
@@ -14,20 +19,16 @@ const App = () => {
     : products;
 
   return (
-    <div id="container">
-      <main id="main-content">
-        <h1>Product Catalog</h1>
-        <div className="content-wrapper">
-          <SideNav
-            categories={categories}
-            series={series}
-            onCategorySelect={setSelectedCategory}
-            selectedCategory={selectedCategory}
-          />
-          <ProductGrid products={filteredProducts} />
-        </div>
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout categories={categories} onCategorySelect={setSelectedCategory} selectedCategory={selectedCategory} />}>
+          <Route index element={<ProductGrid products={filteredProducts} />} />
+          <Route path="product/:productId" element={<ProductDetails />} />
+          <Route path="about" element={<About />} />
+          <Route path="feedback" element={<Feedback />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
